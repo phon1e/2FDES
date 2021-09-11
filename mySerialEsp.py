2106cf695b595189863b52ea692368104b964cb9
phon1e <wiroon2543@gmail.com>
2021-09-11 13:59:07 +0700

    Update mySerialEsp.py

--- mySerialEsp.py
+++ mySerialEsp.py
@@ -1,16 +1,12 @@
-import serial
-import time
+import serial, time, threading 
 
-def getQ():
-  device = serial.Serial(port='/dev/ttyUSB0', baudrate = 9600, timeout =.1)
-  queue =[]*5
-  
-  def write_read():
-    time.sleep(0.5)
-    data = device.readline()
-    return data
-  while True:
-    value = write_read()
-    queue.append((value.decode("utf-8").strip()))
-    print(queue[-3:])
-    print('current queue is' + str(newQ[-1]))
+device = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
+queue = []*5 #create list q size 5
+
+def runQ():
+  time.sleep(0.5)
+  data = device.readline() #read line from serial monitor
+  queue.append((data.decode('utf-8').strip())) #cut space and some byte format off 
+  newQ = list(dict.fromkeys(queue[-3:]))  #remove same value in list and choose lastest 3 element 
+  print('Queue list: ' + str(queue[-3:])) #print lastest 3 element in list 
+  return newQ[-1] #return lastest 1 element in list 
