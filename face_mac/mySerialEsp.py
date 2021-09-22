63a88502bc24139ce3e12b3dd819ca3d58edca96
phon1e <wiroon2543@gmail.com>
2021-09-22 18:18:29 +0700

    Update mySerialEsp.py

--- face_mac/mySerialEsp.py
+++ face_mac/mySerialEsp.py
@@ -7,8 +7,8 @@ queue = []*3 # queue 3
 def runQ():
     time.sleep(0.5)
     data = device.readline()
-    queue.append((data.decode('utf-8').strip()))
-    newQ = list(dict.fromkeys(queue[-3:]))
-    print('Queue list : ' + str(queue[-3:]))
+    queue.append((data.decode('utf-8').strip())) #decode from binary to int
+    newQ = list(dict.fromkeys(queue[-3:]))  #create queue not fill same number
+    print('Queue list : ' + str(queue[-3:])) #print lastest detected num
     #print('current queue is '+ newQ[-1])
-    return newQ[-1]
+    return newQ[-1] #return lastest number in queue
