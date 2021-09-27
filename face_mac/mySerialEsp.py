1ca8314db222ed7414460f282c1330ea5263d2d3
phon1e <wiroon2543@gmail.com>
2021-09-27 09:41:04 +0700

    Update mySerialEsp.py

--- face_mac/mySerialEsp.py
+++ face_mac/mySerialEsp.py
@@ -1,18 +1,5 @@
 import serial, time
-'''
-#def startEsp():
-device = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
-queue = []*3 # queue 3
 
-def runQ():
-    time.sleep(0.5)
-    data = device.readline()
-    queue.append((data.decode('utf-8').strip()))
-    newQ = list(dict.fromkeys(queue[-3:]))
-    print('Queue list : ' + str(queue[-3:]))
-    #print('current queue is '+ newQ[-1])
-    return newQ[-1]
-'''
 portName = '/dev/ttyUSB0'
 max_queue_size = 3
 queue = []*max_queue_size
@@ -34,3 +21,17 @@ def runQ():
     print('Queue list : ' + str(queue[-3:])) #print lastest detected num
     #print('current queue is '+ newQ[-1])
     return newQ[-1] #return lastest number in queue
+'''
+#def startEsp():
+device = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=.1)
+queue = []*3 # queue 3
+
+def runQ():
+    time.sleep(0.5)
+    data = device.readline()
+    queue.append((data.decode('utf-8').strip()))
+    newQ = list(dict.fromkeys(queue[-3:]))
+    print('Queue list : ' + str(queue[-3:]))
+    #print('current queue is '+ newQ[-1])
+    return newQ[-1]
+'''
