4d473f8f68acdd7dd3c2fd378ed1f7976f215729
phon1e <wiroon2543@gmail.com>
2021-11-20 10:51:42 +0700

    new queue, string format

---
+++ face_mac/mySerialEsp_v2.py
@@ -0,0 +1,30 @@
+import serial, time, collections
+
+# portName = '/dev/ttyUSB0'
+# portName = 'COM5'
+max_size = 5
+queue = collections.deque([]*max_size, maxlen=max_size)
+device = None
+
+def portAvailalbe():
+    try:
+        global device
+        device = serial.Serial(port=portName, baudrate=9600, timeout=.1)
+        return True
+    except:
+        return False
+
+def runQ():
+    time.sleep(1)
+    data = device.readline()
+    queue.append((data.decode('utf-8').strip())) #decode from binary to int
+    print(f'Queue list : {str(list(queue))}')
+    #print(f'returned {queue[0]}')
+    return queue[0]
+
+def test_run():
+    portAvailalbe()
+    while True:
+        runQ()
+
+# test_run()
