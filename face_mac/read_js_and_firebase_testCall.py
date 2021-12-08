fd7f883fee226c11a6ce5503e4ac3cacf368a8ea
phon1e <wiroon2543@gmail.com>
2021-12-08 17:45:34 +0700

    added execution time
    between local(json file) and from sv(firebase) on win10

--- face_mac/read_js_and_firebase_testCall.py
+++ face_mac/read_js_and_firebase_testCall.py
@@ -1,5 +1,6 @@
-import mySvModule
+import mySvModule, time
 
+start1 = time.time()
 
 url = 'https://sendfrompytest-default-rtdb.firebaseio.com/'
 key = '-MqHz5m_algE-XHj4WXM'
@@ -12,15 +13,18 @@ i = 0
 ids = '4444444443'
 print("Read from firebase")
 
+
 for i in range(3):
     if ids in d[i]['mac']:
           print(f'found {ids} in {d[i]["mac"]}')
           print(f'checked {d[i]["username"]}')
+end1 = time.time()
+print(f'execution time {end1 - start1} sec' )
 
 # output
-#Read from firebase
-#found 4444444443 in ['0000000001', '4444444443', '4444444445']
-#checked 044_uname
+# found 4444444443 in ['0000000001', '4444444443', '4444444445']
+# checked 044_uname
+start2 = time.time()
 
 db = mySvModule.readJs("data.json")
 print("\nRead from json")
@@ -29,7 +33,14 @@ for i in range(3):
         print(f'Found {ids} in { db["users"][str(i)]["mac"]}' )
         print(f'checked {db["users"][str(i)]["username"]}')
 
-#output
-#Read from json
-#Found 4444444443 in ['0000000001', '4444444443', '4444444445']
-#checked 044_uname
+end2 = time.time()
+print(f'execution time {end2 - start2} sec' )
+
+# output
+#     Found
+#     4444444443 in ['0000000001', '4444444443', '4444444445']
+#     044
+#     _uname
+
+
+
