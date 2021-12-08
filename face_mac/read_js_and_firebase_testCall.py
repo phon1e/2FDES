6f172d55075af9e7d981d786ad4edbf3bb3f1ef5
phon1e <wiroon2543@gmail.com>
2021-12-08 17:27:20 +0700

    custom_db_sv_module_test_call

---
+++ face_mac/read_js_and_firebase_testCall.py
@@ -0,0 +1,35 @@
+import mySvModule
+
+
+url = 'https://sendfrompytest-default-rtdb.firebaseio.com/'
+key = '-MqHz5m_algE-XHj4WXM'
+path = f'/dbUser/{key}/users'
+
+
+d = mySvModule.readDb(url,path)
+
+i = 0
+ids = '4444444443'
+print("Read from firebase")
+
+for i in range(3):
+    if ids in d[i]['mac']:
+          print(f'found {ids} in {d[i]["mac"]}')
+          print(f'checked {d[i]["username"]}')
+
+# output
+# found 4444444443 in ['0000000001', '4444444443', '4444444445']
+# checked 044_uname
+
+db = mySvModule.readJs("data.json")
+print("\nRead from json")
+for i in range(3):
+    if ids in db['users'][str(i)]['mac']:
+        print(f'Found {ids} in { db["users"][str(i)]["mac"]}' )
+        print(f'checked {db["users"][str(i)]["username"]}')
+
+# output
+#     Found
+#     4444444443 in ['0000000001', '4444444443', '4444444445']
+#     044
+#     _uname
