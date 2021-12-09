7fcb101f9fda6940ce6627af46e470b133313f76
phon1e <wiroon2543@gmail.com>
2021-12-09 09:30:20 +0700

    added output example

--- face_mac/testCall.py
+++ face_mac/testCall.py
@@ -19,10 +19,6 @@ def test():
     end1 = time.time()
     diff1 = end1 - start1
     print(f'execution time {diff1:.6f} sec' )
-
-    # output
-    # found 4444444443 in ['0000000001', '4444444443', '4444444445']
-    # checked 044_uname
     start2 = time.time()
 
 
@@ -40,14 +36,6 @@ def test():
 
 
     print(f"\ndiff read time between local and server : {(diff1 - diff2):.6f} sec")
-    # output
-    #     Found
-    #     4444444443 in ['0000000001', '4444444443', '4444444445']
-    #     044
-    #     _uname
-
-    #print(f'read from firebase db {d}')
-    #print(f'read from loaded file {js}')
 
 # update mac to db
 def test2():
@@ -61,3 +49,30 @@ def test2():
         c+=1
 test2()
 # test()
+
+'''             
+###################### test() output on window and pi4 ##################### 
+output:
+Read from firebase
+found 0xfc,0x1d,0x43,0x31,0x94,0xc5 in ['0xe6,0xbc,0x50,0xb3,0x29,0xc6', '0x12,0xa9,0x67,0x05,0xb6,0xf9', '0xfc,0x1d,0x43,0x31,0x94,0xc5']
+checked 415_uname
+execution time 0.866708 sec
+
+Read from json
+Found 0xfc,0x1d,0x43,0x31,0x94,0xc5 in ['0xe6,0xbc,0x50,0xb3,0x29,0xc6', '0x12,0xa9,0x67,0x05,0xb6,0xf9', '0xfc,0x1d,0x43,0x31,0x94,0xc5']
+checked 415_uname
+execution time 0.001995 sec
+
+diff read time between local and server : 0.864713 sec
+
+################# test2() output ###################
+output:
+
+updated
+ 0xe6,0xbc,0x50,0xb3,0x29,0xc6
+updated
+ 0x12,0xa9,0x67,0x05,0xb6,0xf9
+updated
+ 0xfc,0x1d,0x43,0x31,0x94,0xc5
+
+'''
