934dcfde9471aa36757b27e8e11af960eae78245
phon1e <wiroon2543@gmail.com>
2022-01-06 08:38:35 +0700

    added mapping func list to dict

--- face_mac/mySvModule.py
+++ face_mac/mySvModule.py
@@ -1,20 +1,20 @@
 from firebase import firebase as fb
 import json
 
-def insertDb(url,path, myData, key):
+def insertDb(url,path, myData, key):    #add new db to server
     if all(isinstance(i, str) for i in [url, path, myData, key]):
         firebase = fb.FirebaseApplication(url, None)
         result = firebase.post(path, myData)
     raise TypeError('All must be String ! !')
 
-def readDb(url,path):
+def readDb(url,path):   #read db from server
     if all(isinstance(i, str) for i in [url, path]):
         firebase = fb.FirebaseApplication(url, None)
         result = firebase.get(path, '')
         return result
     raise TypeError('All must be String ! !')
 
-def updateDb(url, path, user, key, val ):
+def updateDb(url, path, user, key, val ):   #update value in db
     # if all(isinstance(i, str) for i in [url, path, user, key, val]):
     firebase = fb.FirebaseApplication(url, None)
     result = firebase.put(f'{path}/{user}', key, val)
@@ -22,26 +22,26 @@ def updateDb(url, path, user, key, val ):
     #print(f"{type(url)} {type(path)} {type(user)} {type(key)} {type(val)}")
     # else: print('All must be String ! !')
 
-def deleteDb(url, path, key):
+def deleteDb(url, path, key):   #delete specific path in db
     if all(isinstance(i, str) for i in [url, path,key]):
         firebase = fb.FirebaseApplication(url, None)
         result = firebase.delete(path, key)
         print('record deleted')
     raise TypeError('All must be String ! !')
 
-def readJs(filename):
+def readJs(filename):   #read json file
    if (isinstance(filename,str)):
         with open(filename) as json_file:
             d = json.load(json_file)
         return d
    raise TypeError('All must be String ! !')
 
-def loadJs(data, filename, ind):
+def loadJs(data, filename, ind):    #load json file from server and save as new file.
     json_object = json.dumps(data, indent= ind)
     with open(filename, 'w', encoding='utf-8') as outfile:
         outfile.write(json_object)
 
-def getKeyDict(ls, indx):
+def getKeyDict(ls, indx):   # input ls to auto create dict and return key of specific indx.
     enum = enumerate(ls)
     d = dict((i,j) for j,i in enum)
     k = list(d.keys())[list(d.values()).index(indx)]
