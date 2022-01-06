8125beb0d4b38745c7bc94a52fb5e1fbe41773eb
phon1e <wiroon2543@gmail.com>
2022-01-06 11:35:37 +0700

    Update mySvModule.py

--- face_mac/mySvModule.py
+++ face_mac/mySvModule.py
@@ -2,35 +2,29 @@ from firebase import firebase as fb
 import json
 
 def insertDb(url,path, myData, key):    #add new db to server
-    if all(isinstance(i, str) for i in [url, path, myData, key]):
-        firebase = fb.FirebaseApplication(url, None)
-        result = firebase.post(path, myData)
-    raise TypeError('All must be String ! !')
+    firebase = fb.FirebaseApplication(url, None)
+    result = firebase.post(path, myData)
+
 
 def readDb(url,path):   #read db from server
-    if all(isinstance(i, str) for i in [url, path]):
-        firebase = fb.FirebaseApplication(url, None)
-        result = firebase.get(path, '')
-        return result
-    raise TypeError('All must be String ! !')
+    firebase = fb.FirebaseApplication(url, None)
+    result = firebase.get(path, '')
+    return result
+
 
 def updateDb(url, path, user, key, val ):   #update value in db
-    # if all(isinstance(i, str) for i in [url, path, user, key, val]):
     firebase = fb.FirebaseApplication(url, None)
     result = firebase.put(f'{path}/{user}', key, val)
     print(f'updated\n {result}')
-    #print(f"{type(url)} {type(path)} {type(user)} {type(key)} {type(val)}")
-    # else: print('All must be String ! !')
+
 
 def deleteDb(url, path, key):   #delete specific path in db
-    if all(isinstance(i, str) for i in [url, path,key]):
-        firebase = fb.FirebaseApplication(url, None)
-        result = firebase.delete(path, key)
-        print('record deleted')
-    raise TypeError('All must be String ! !')
+    firebase = fb.FirebaseApplication(url, None)
+    result = firebase.delete(path, key)
+    print('record deleted')
 
 def readJs(filename):   #read json file
-   if (isinstance(filename,str)):
+    if (isinstance(filename,str)):
         with open(filename) as json_file:
             d = json.load(json_file)
         return d
@@ -42,7 +36,7 @@ def loadJs(data, filename, ind):    #load json file from server and save as new 
         outfile.write(json_object)
 
 def getKeyDict(ls, indx):   # input ls to auto create dict and return key of specific indx.
-    enum = enumerate(ls,1) # start at 1
+    enum = enumerate(ls)
     d = dict((i,j) for j,i in enum)
     k = list(d.keys())[list(d.values()).index(indx)]
     return k
