2f612a914020cdd38bc210bdc5e2f421c5517b68
phon1e <wiroon2543@gmail.com>
2022-01-13 08:49:00 +0700

    Update mySvModule.py

--- face_mac/customModule/mySvModule.py
+++ face_mac/customModule/mySvModule.py
@@ -1,6 +1,9 @@
 from firebase import firebase as fb
-import json, firebase_admin
-import pyrebase
+import json
+import pyrebase, firebase_admin
+from firebase_admin import firestore
+from datetime import datetime
+import calendar
 
 def init():
     firebaseConfig = readJs('config.json')
@@ -13,14 +16,31 @@ def insertDb(url,path, data):
     result = firebase.put(path,"users", data)
 
 
-def readDb(config_file):
+def readDb():
     db = init()
     return db.get()
 
-def timestamp(user_id, record, time):
+def timestamp(user_id):
+
     db = init()
-    d = db.child('users').child(f"{user_id}").child('timestamp').update({f"record{record}":f"{time}"})
-    print(f'updated',d)
+    now = datetime.now()    #get current datatime 
+    dt = now.strftime("%d/%m/%Y %H:%M") # format datetime
+    last_day_in_month = calendar.monthrange(now.year, now.month)[1] # check how many day in current month
+    last_time_stamp = db.child("users").child(user_id).child("timestamp").order_by_key().get() # get timestamp list
+    
+    if (last_time_stamp.val() is None): # if no timestamp add record1
+        record = 1
+        d = db.child('users').child(user_id).child('timestamp').update({f'record{record}': f"{dt}"})
+    else:
+        last_record =  list(last_time_stamp.val())[-1] #get lastest timestamp 
+        pos = last_record[6:]   #get lastest no of timestamp eg. record30 got "30" 
+        record = int(pos) + 1   # last record + 1 for next timestamp eg. record30 --> record31
+        
+        if(record >= last_day_in_month):    #check if lastest record >= days in current month then reset record to 1
+            record = 1
+            
+        d = db.child('users').child(user_id).child('timestamp').update({f'record{record}': f"{dt}"})
+
 
 
 def deleteDb(url, path, key):
@@ -49,7 +69,9 @@ def getDictKey(ls, indx):
     k = list(d.keys())[list(d.values()).index(indx)]
     return k
 
-def uploadImg(config_file, folder, filename, file):
-    config = readJs(config_file)
-    firebase = pyrebase.initialize_app(config)
+def uploadImg(folder, filename, file):
+    firebaseConfig = readJs('config.json')
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    storage = firebase.storage()
+
     storage.child(f"{folder}/{filename}").put(file)
