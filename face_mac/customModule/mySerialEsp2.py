746a9932a0ef6e7becfe27870342a66b63503282
phon1e <wiroon2543@gmail.com>
2022-03-07 21:13:23 +0700

    Add files via upload

--- face_mac/customModule/mySerialEsp2.py
+++ face_mac/customModule/mySerialEsp2.py
@@ -14,19 +14,30 @@ def portAvailalbe():
     except:
         return False
 
+def getQ():
+    if len(queue) > 0:
+        return queue[0]
+    else:
+        return "null"
+
 def runQ():
-    time.sleep(1)
-    data = device.readline()
-    queue.append((data.decode('utf-8').strip())) #decode from binary to int
-    items = list(dict.fromkeys(queue))
-    print(f'Queue list : {str(items)}')
-    #print(f'returned {queue[0]}')
-    return queue[0]
+    while True:
+        data = device.readline()
+        data = data.decode('utf-8').strip() #decode from binary to int
+        if(data != b'' or data != ""):
+            queue.append(data) 
+            print(f"data : {data}")
+            #print(queue[0])
+            #return queue[0]
+        #else:
+            #return "null"
+        time.sleep(0.2)
 
 def test_run():
     portAvailalbe()
     while True:
         runQ()
-   
-
+        
 #test_run()
+
+
