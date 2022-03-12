5b0c6a43b1584a8db0d889ab227c1d925b76a713
phon1e <wiroon2543@gmail.com>
2022-03-12 16:21:00 +0700

    Update start.py

--- face_mac/start.py
+++ face_mac/start.py
@@ -1,31 +1,27 @@
 import os
 
+def print_txt(s):
+  print("="*30)
+  print(f"\n {s}\n")
+  print("="*30)
 
 main_dir = "/home/pi/Desktop/2fdes"
 sub_dir = "/home/pi/Desktop/mkspiffs/mkspiffs-0.2.3-7-gf248296-arduino-esp8266-"
 
 os.chdir(main_dir)
-
 print_txt("LOADING AND MAPPING MAC ...")
 
 os.system("python mapping_mac.py")
-
 os.system('''./mkspiffs -b 0x1000 -p 0x100 -s 0x00200000 -d 5 --create ./data spiffs.bin
 ''')
-
 os.system('''esptool.py --baud 921600 --port /dev/ttyUSB0 write_flash 0x00200000 ./spiffs.bin -u
 ''')
 
 os.chdir(sub_dir)
 print_txt("UPLOADING DATA TO ESP8266. . . ")
-
-
 os.chdir(main_dir)
 
 print_txt(" STARTING THE MAIN SYSTEM. . .")
 os.system("python mFileV12_1.py")
 
-def print_txt(s):
-  print("="*30)
-  print(f"\n {s}\n")
-  print("="*30)
+)
